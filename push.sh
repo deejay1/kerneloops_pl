d75726041e4e5e648127530ccf2d1640dbd2acd5
Arjan van de Ven <arjan@infradead.org>
2008-01-10 18:04:56 -0800

    also push the tags out

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 git gc
 git-push -f ssh://git.infradead.org/srv/git/kerneloops.git master:refs/heads/master
+git-push --tags -f ssh://git.infradead.org/srv/git/kerneloops.git master:refs/heads/master
